e42b142e0f6d2e25ae081db3bfc284eece36b66a
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2020-12-09 20:48:56 +0100

    commmit

--- js/init.js
+++ js/init.js
@@ -12,9 +12,10 @@ $(document).ready(function() {
  gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
  gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
  gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
- gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
- gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
- gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+ gsap.from(".header h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+ gsap.from(".header h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+ gsap.from(".header h5", { opacity: 0, duration: 1, delay: 3, y: -45 });
+ gsap.from(".header a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
 
 (function($){
   $(function(){
